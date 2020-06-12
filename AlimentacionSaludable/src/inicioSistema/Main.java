a81cc1b4669cc6d95186d406e3cd32ae836b43a6
Juan C <jcasanova.s.j.b@gmail.com>
2020-06-11 23:37:49 -0300

    Pasaje a SPA

---
+++ AlimentacionSaludable/src/inicioSistema/Main.java
@@ -0,0 +1,27 @@
+package inicioSistema;
+
+import dominio.Sistema;
+import interfaz.VentanaMenuPrincipal;
+import interfaz.VentanaSPA;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Sistema sistema;
+
+        try {
+            try (FileInputStream f = new FileInputStream("Sistema.data")) {
+                sistema = (Sistema) new ObjectInputStream(new BufferedInputStream(f)).readObject();
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            sistema = new Sistema();
+        }
+        VentanaSPA ventana = new VentanaSPA(sistema);
+        ventana.setVisible(true);
+    }
+
+}
