f11c3f8483a9fb7b567a35150b270f7ae9518a20
Juan C <jcasanova.s.j.b@gmail.com>
2020-06-15 05:54:17 -0300

    Mejorado el menu de crear plan alimenticio

---
+++ src/Dominio/ComposicionAlimento.java
@@ -0,0 +1,41 @@
+package Dominio;
+
+import java.io.Serializable;
+
+public final class ComposicionAlimento implements Serializable {
+
+    private String nutriente;
+    private float proporcion;
+
+    public ComposicionAlimento(String unNutriente, float unaProporcion) {
+        if (unNutriente == null || unNutriente.trim().equals("")){
+            this.setNutriente("No definido");
+        } else {
+            this.setNutriente(unNutriente);   
+        }
+        this.setProporcion(unaProporcion);
+    }
+
+    public String getNutriente() {
+        return this.nutriente;
+    }
+
+    public void setNutriente(String unNutriente) {
+        this.nutriente = unNutriente;
+
+
+    }
+
+    public float getProporcion() {
+        return this.proporcion;
+    }
+
+    public void setProporcion(float unaProporcion) {
+        if (unaProporcion <= 0) {
+            this.proporcion = 1;
+        } else {
+            this.proporcion = unaProporcion;
+        }
+    }
+
+}
